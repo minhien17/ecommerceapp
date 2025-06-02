d100486e67aa801d49778d0f479e4f78a8a82b9e
sieupkcool <vietanh10120@gmail.com>
2025-06-02 15:33:28 +0700

    product api

--- BE/apps/products/urls.py
+++ BE/apps/products/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import product_detail, product_list, review
 
 urlpatterns = [
-    # Các endpoints sẽ thêm sau
+    path('', product_list, name='product_list'),  # GET /api/products
+    path('<str:productid>', product_detail, name='product_detail'),  # GET /api/products/<productid>
+    path('review/<str:productid>', review, name='review'),  # GET & POST /api/products/review/<productid>
 ]
