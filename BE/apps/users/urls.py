7b1b08341de53c64925d05e155642777240a1f83
sieupkcool <vietanh10120@gmail.com>
2025-06-02 15:07:57 +0700

    api user

--- BE/apps/users/urls.py
+++ BE/apps/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import login, get_users
+from .views import login, get_users, signup, cart, remove_from_cart, update_user
 
 urlpatterns = [
     path('login', login, name='login'),
-    path('getuser', get_users, name='getuser')
+    path('getuser', get_users, name='getuser'),
+    path('signup', signup, name='signup'),
+    path('cart', cart, name='cart'),
+    path('cart/<str:productid>', remove_from_cart, name='remove_from_cart'),
+    path('', update_user, name='update_user')
 ]
--- BE/apps/users/views.py
+++ BE/apps/users/views.py
@@ -2,6 +2,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+def api_response(data=None, message="", code=200, status=200, errMessage=""):
+    return Response({
+        "message": message,
+        "code": code,
+        "data": data,
+        "status": status,
+        "errMessage": errMessage
+    }, status=status)
+
 # API login
 @api_view(['POST'])
 def login(request):
@@ -46,3 +55,54 @@ def get_users(request):
     ]
     
     return Response(users, status=status.HTTP_200_OK)
+
+#sign up
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    password = request.data.get('password')
+    # TODO: Thêm kiểm tra và lưu user thực tế
+    return api_response(data={"is_success": "true"}, message="Signup success", code=201, status=201)
+
+#cart
+@api_view(['GET', 'POST'])
+def cart(request):
+    if request.method == 'GET':
+        # ... code lấy giỏ hàng ...
+        cart = [
+            {
+                "product_id": "1",
+                "quantity": 2,
+                "product": {
+                    "images": "https://example.com/image1.jpg",
+                    "discount_price": 9.0,
+                    "title": "Áo thun nam"
+                }
+            }
+        ]
+        return api_response(data=cart, message="Get cart success", code=200, status=200)
+    elif request.method == 'POST':
+        # ... code thêm sản phẩm vào giỏ ...
+        return api_response(data={"success": True}, message="Add to cart success", code=200, status=200)
+
+#xoa sp
+@api_view(['DELETE'])
+def remove_from_cart(request, productid):
+    # TODO: Xóa sản phẩm khỏi cart thực tế
+    cart = []
+    return api_response(data=cart, message="Remove from cart success", code=200, status=200)
+
+#cap nhat thong tin ca nhan
+@api_view(['PATCH'])
+def update_user(request):
+    # TODO: Lấy user từ token, cập nhật thông tin
+    if request.data.get("password") == "wrong":
+        return api_response(data={"success": False}, message="Invalid password", code=400, status=400, errMessage="INVALID_PASSWORD")
+    user = {
+        "id": "user_id",
+        "picture": request.data.get("picture"),
+        "name": request.data.get("name"),
+        "number": request.data.get("number")
+    }
+    return api_response(data={"success": True, "user": user}, message="Update user success", code=200, status=200)
