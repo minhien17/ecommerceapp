585ca98e25bfd181115d180c9ea1d7c0e08681b6
Nguyễn giang Linh <126006181+myLovetory@users.noreply.github.com>
2025-06-14 08:12:52 +0700

    Update urls.py

--- BE/apps/users/urls.py
+++ BE/apps/users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('cart/<str:productid>', add_to_cart, name='add_to_cart'),  # POST
     path('cart/<str:productid>/increase', increase_cart_item, name='increase_cart_item'),  # POST
     path('cart/<str:productid>/decrease', decrease_cart_item, name='decrease_cart_item'),  # POST
-    path('cart/<str:productid>', remove_from_cart, name='remove_from_cart'),
+    path('cart/<str:productid>/remove', remove_from_cart, name='remove_from_cart'),
     path('update', update_user, name='update_user'),
     path('changepw', change_password, name='change_password'),
     
